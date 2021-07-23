87ee936a076a837de9be68e68c7f2e58db432380
AlexZhadaev <pvt.vatnik@gmail.com>
2021-07-23 16:01:45 +0300

    Project created

---
+++ test_app/test_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_app
+//
+//  Created by Жадаев Алексей on 23.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
